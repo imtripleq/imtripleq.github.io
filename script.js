848fcff3ccbb726d2b98e211a9729794008f0b37
imtripleq <flamework89@hotmail.com>
2021-10-30 11:35:34 +1100

    updated

---
+++ script.js
@@ -0,0 +1,52 @@
+/////////////////////////////////
+////EXERCISE 1
+/////////////////////////////////
+
+// let season = "summer";
+// const raining = true;
+// if (season === "winter") console.log("Its cold");
+// else if (season === "summer") console.log("Its hot");
+// else {
+//   if (raining) {
+//     console.log("Too bad its raining, we should stay home");
+//   } else {
+//     console.log("Its a good weather today,we should go out");
+//   }
+// }
+
+// ////////////////////////////////
+// ////EXERCISE 2
+// /////////////////////////////////
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(`${i}`);
+//   if (i % 10 === 0) {
+//     console.log(`I'm divisible by 10`);
+//   }
+// }
+////////////////////////////////////////////////
+/////EXERCISE 3
+////////////////////////////////////////////////
+
+// let i = 0;
+
+// for (let i = 0; i < 6; i++) {
+//   let row = "";
+//   for (let q = 0; q < i + 1; q++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+///////////////////////
+///////EXERCISE 4
+///////////////////////
+// let n = 6;
+
+// for (let i = 0; i < n; i++) {
+//   let row = "I'm the first loop";
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + " nested loop";
+//   }
+//   console.log(row);
+// }
